d241867f5a1ced409c0df1bcb9bea0bc71408efb
Jo Blaszak <jblaszak@gmail.com>
2021-09-30 23:02:17 -0700

    Added constants to generalize the code for different image sizes and some code for eventual initialization of pixel attributes!

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const MAX_WIDTH = 100;
+export const MAX_WIDTH_SQUARED = 10000;
--- src/store/dataMap-actions.js
+++ src/store/dataMap-actions.js
@@ -1,6 +1,11 @@
+import { set, ref } from "firebase/database";
+import db from "../services/firebase";
+
 import { dataMapActions } from "./dataMap-slice";
 import { errorActions } from "./error-slice.js";
 import { imageColorData } from "../assets/imageColorData";
+
+import * as CONSTANTS from "../constants";
 
 export const loadPixelData = () => {
   return async (dispatch) => {
@@ -11,11 +16,73 @@ export const loadPixelData = () => {
       if (storageData) {
         pixelData = JSON.parse(storageData);
         // localStorage.clear();
+
+        //// Generate some dummy data in firebase
+        let pixelStats = {};
+        for (let i = 1; i <= CONSTANTS.MAX_WIDTH_SQUARED; i++) {
+          pixelStats[i] = {
+            p: 0,
+            c: "ETH",
+            s: 0,
+            u: "No data",
+            a: "No data",
+          };
+        }
+        // await updateDoc(doc(db, "pixels", "pixelStats"), pixelStats);
+
+        let diagPixels = [];
+        let edgePixels = [];
+        let fortyTwoPixels = [];
+
+        for (let i = 1; i <= CONSTANTS.MAX_WIDTH; i++) {
+          diagPixels.push((i - 1) * CONSTANTS.MAX_WIDTH); // '\' diagonal
+          diagPixels.push(i * CONSTANTS.MAX_WIDTH - i + 1); // '/' diagonal
+          edgePixels.push(i); // top edge
+          edgePixels.push(CONSTANTS.MAX_WIDTH * (CONSTANTS.MAX_WIDTH - 1) + i); // bottom edge
+          if (i !== 1 || i !== CONSTANTS.MAX_WIDTH) {
+            edgePixels.push(1 + (i - 1) * CONSTANTS.MAX_WIDTH); // left edge
+            edgePixels.push((i - 1) * CONSTANTS.MAX_WIDTH); // right edge
+          }
+          fortyTwoPixels.push(42 + (i - 1) * 100); // vertical 42 pixels
+          const horizontal42 = 42 * CONSTANTS.MAX_WIDTH + i;
+          if (fortyTwoPixels.indexOf(horizontal42) === -1) {
+            fortyTwoPixels.push(horizontal42); // horizontal 42 pixels
+          }
+        }
+
+        let pixelAttributes = {};
+        for (let i = 1; i <= CONSTANTS.MAX_WIDTH_SQUARED; i++) {
+          // Calculate distance to center
+          const x = (i - 1) % CONSTANTS.MAX_WIDTH;
+          const y = Math.floor((i - 1) / CONSTANTS.MAX_WIDTH);
+          let mid = CONSTANTS.MAX_WIDTH / 2 - 0.5;
+          let d2c = 0;
+
+          if (CONSTANTS.MAX_WIDTH % 2) {
+            mid = Math.floor(mid);
+            const xDist = Math.abs(x - mid);
+            const yDist = Math.abs(y - mid);
+            d2c = Math.max(xDist, yDist);
+          } else {
+            const xDist = Math.abs(x - mid);
+            const yDist = Math.abs(y - mid);
+            d2c = Math.floor(Math.max(xDist, yDist));
+          }
+
+          pixelAttributes[i] = {};
+        }
+
+        // set(ref(db, "pixels/"), null);
+
+        // const unsub = onSnapshot(doc(db, "pixels", "pixelData"), (doc) => {
+        //   console.log("Current data: ", doc.data());
+        // });
+
         console.log("Loaded data from storage!");
       } else {
         pixelData = imageColorData.map((color) => ({
           color: color,
-          lastPrice: "No data",
+          lastPrice: 0,
           priceUnit: "ETH",
           timesSold: Math.floor(Math.random() * 10),
           ownerUsername: "No data",
